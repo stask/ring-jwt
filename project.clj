adbdf1cd453698a85d58610e0474949119c7d946
stask <stask@me.com>
2014-01-15 16:23:04 +0200

    updated name

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject ring-jwt "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject stask/ring-jwt "0.1.0-SNAPSHOT"
+  :description "(too) simple JWT implementation"
+  :url "https://github.com/stask/ring-jwt"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.3.3"
